ca1c729ca7a613d47d689e357d529777f485a400
lixiaoming <lixiaoming@renrenche.com>
2019-06-13 14:45:33 +0800

    add redis config

--- src/main/java/lxm/service/TestService.java
+++ src/main/java/lxm/service/TestService.java
@@ -2,15 +2,20 @@ package lxm.service;
 
 import lxm.mapper.TestMapper;
 import lxm.model.Test;
+import lxm.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
     @Autowired
     private TestMapper testMapper;
+    @Autowired
+    private RedisUtil redisUtil;
 
-    public Test queryById(String id){
+    public Test queryById(String id) {
+        System.out.println(redisUtil.get("test"));
         return testMapper.selectByPrimaryKey(Integer.parseInt(id));
     }
 }
